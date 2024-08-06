a232a1ea9674d81e37938e973172503db0ad6afb
koirherianto <koir.harianto@gmail.com>
2024-08-06 17:08:38 +0800

    d

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'apisaga',
-      script: './bin/server.js',
+      script: './bin/server.ts',
       instances: 'max',
       exec_mode: 'cluster',
       autorestart: true,
