54b4b9edf3fc859e83ac39c12d395089ac3a8975
koirherianto <koir.harianto@gmail.com>
2024-08-06 17:04:26 +0800

    chore: Refactor ecosystem.config.js and update app configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,7 @@
-// export default {
-//   apps: [
-//     {
-//       name: 'apisaga',
-//       script: './server.js',
-//       instances: 'max',
-//       exec_mode: 'cluster',
-//       autorestart: true,
-//     },
-//   ],
-// }
-
 module.exports = {
   apps: [
     {
-      name: 'web-app',
+      name: 'apisaga',
       script: './server.js',
       instances: 'max',
       exec_mode: 'cluster',
